1b942c4e39341b8a0e6973d32ba8b3323f42b638
Wojciech Mojsiejuk <wojciechmojsiejuk@MacBook-Pro-Wojciech.local>
2019-06-07 16:49:18 +0200

    working hairdresser

--- sh1/main.c
+++ sh1/main.c
@@ -1,13 +1,14 @@
+#define _DEFAULT_SOURCE
+#include <semaphore.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
 #include <string.h>
-#include <stdlib.h>
 #include <limits.h>
 #include <unistd.h>
+#include <time.h>
+#include "queue.h"
 /*
-TO DO:
-ZAPYTAĆ JAK MAJĄ BYĆ TWORZENI KLIENCI
-
 Salon fryzjerski składa się z gabinetu z jednym
 fotelem (zasób wymagający synchronizacji)
 oraz z poczekalni zawierającej n krzeseł.
@@ -60,87 +61,152 @@ Za każdy przypadek potencjalnego wyścigu -3p.
 
 //1 - running in debug mode
 int debug = 0;
-//chairs
+//total chairs
 long int N;
-//waiting rooms available chairs
-long int chairsAvailable;
+//If hairdresser is busy (1) or not (0)
+sem_t hairdressersChairTaken;
+//If waiting room queue is empty(1) or not (0)
+sem_t queueEmpty;
+//total clients count
+long int totalClientsCount;
+// blocks clientsResignationCount value
+pthread_mutex_t readingResigned = PTHREAD_MUTEX_INITIALIZER;
 //how many clients resigned
-long int clientsResignationCount = 0;
-//
+int clientsResignationCount = 0;
+
 pthread_mutex_t hairdressersChair = PTHREAD_MUTEX_INITIALIZER;
+//currently cut client
+int currentlyCutClientId=-1;
+//blocks current_queue_size value
+pthread_mutex_t readingQueue = PTHREAD_MUTEX_INITIALIZER;
+//clients queue: waiting in waiting room
+struct Queue waitingRoom;
+//clients
 
-
-
+//pthread_mutex_t readingQueue, readingResigned , hairdressersChair
+void show_message()
+{
+    printf("Res:%d WRomm: %d/%ld [in: %d]",
+    clientsResignationCount,
+    current_queue_size(&waitingRoom),
+    N,
+    currentlyCutClientId);
+}
+void waiting(int sec)
+{
+    int zzz = (((rand()%sec)+1)*1000000);
+    usleep(zzz);
+}
+// TO DO: rename to client
 void* haircut(void* arg)
 {
+  int id;
+  id = *((int *) arg);
   pthread_mutex_lock(&hairdressersChair);
-  long *id;
-  id = (long*)arg;
-  printf("Haircutting... (in: %ld)\n", id);
-  usleep(250*1000);
+  hairdressersChairTaken = 1;
+  currentlyCutClientId=id;
+  if(debug)
+    printf("Haircutting... (client id: %d)\n",id);
+  waiting(5);
+  hairdressersChairTaken = 0;
+  pthread_mutex_lock(&readingQueue);
+  pop(&waitingRoom);
+  if(debug)
+    printf("Leaving hairdresser... (client id: %d)\n",id );
+  pthread_mutex_unlock(&readingQueue);
   pthread_mutex_unlock(&hairdressersChair);
+  return 0;
 }
 
+void* barberCut(struct Client* client)
+{
+  int cuttingTime = 1;
+  //cuttingTime = rand();
+  pthread_mutex_lock(&hairdressersChair);
+  printf("Haircutting... (in: %d)\n", client->id);
+  sleep(cuttingTime);
+  pthread_mutex_unlock(&hairdressersChair);
+  return 0;
+}
+
+//Parameters: total chairs, total client, optional: -debug
 int main(int argc, char* argv[])
 {
-    //Invalid parameter cunt - end program
-    if(argc < 2 || argc > 3)
+    //Invalid parameter count - end program
+    if(argc < 3 || argc > 4)
     {
         fprintf(stderr, "Invalid parameter count\n");
         return 1;
     }
     //Enable debug
-    if(argc == 3 && strcmp(argv[2],"-debug")==0)
+    if(argc == 4 && strcmp(argv[3],"-debug")==0)
     {
         debug = 1;
     }
     if(debug)
         printf("Debug mode enabled\n");
+    //Set all chairs count
     char* endptr;
     N = strtol(argv[1], &endptr,10);
     if(*endptr != '\0' || N < 0 || N == LONG_MIN || N == LONG_MAX)
     {
-      fprintf(stderr, "Invalid N parameter\n");
+      fprintf(stderr, "Invalid total chairs count\n");
       return 2;
     }
-    //Set chairs available quantity to N
-    chairsAvailable = N;
+    //Set totalClientsCount
+    char* endptr2;
+    totalClientsCount = strtol(argv[2], &endptr2, 10);
+    if(*endptr2 != '\0' ||  totalClientsCount< 0 ||  totalClientsCount== LONG_MIN ||  totalClientsCount== LONG_MAX)
+    {
+      fprintf(stderr, "Invalid total clients count\n");
+      return 3;
+    }
+    if(debug)
+        printf("Semaphores initialization\n");
+    sem_init(&queueEmpty,0,1);
+    sem_init(&hairdressersChairTaken,0,0);
     if(debug)
         printf("(debug) N chairs = %ld\n",N);
-    pthread_t* id_array;
-    id_array = (pthread_t*)malloc(sizeof(pthread_t)*N);
-    for(long j=0;j<N;j++)
+    if(debug)
+        printf("Initialize queue\n");
+    init(&waitingRoom);
+    // for(int i=0;i<totalClientsCount;i++)
+    // {
+    //     push(&waitingRoom,i);
+    // }
+    //int x;
+    //Create threads
+    pthread_t* clientID;
+    clientID = (pthread_t*)malloc(sizeof(pthread_t)*totalClientsCount);
+    int* threadID = (int*)malloc(sizeof(int)*totalClientsCount);
+    srand((unsigned)time(NULL));
+    for(int j=0;j<totalClientsCount;j++)
     {
-      id_array[j]=j;
-      pthread_create (&id_array[j], NULL, &haircut,(void*)&id_array[j]);
+        //clients show up to the hairdresser
+        waiting(3);
+        threadID[j]=j;
+        //checking if queue is full
+        pthread_mutex_lock(&readingQueue);
+        if(current_queue_size(&waitingRoom)<N)
+        {
+            push(&waitingRoom,j);
+            pthread_mutex_unlock(&readingQueue);
+            pthread_create (&clientID[j], NULL, &haircut, (void*)&threadID[j]);
+        }
+        else
+        {
+            if(debug)
+                printf("Queue is full. (client id: %d) resigned \n",j);
+            pthread_mutex_lock(&readingResigned);
+            clientsResignationCount++;
+            pthread_mutex_unlock(&readingResigned);
+            pthread_mutex_unlock(&readingQueue);
+        }
+
     }
-    for(long j=0;j<N;j++)
+    for(int j=0;j<totalClientsCount;j++)
     {
-      pthread_join(id_array[j], NULL);
+      pthread_join(clientID[j], NULL);
     }
     exit(EXIT_SUCCESS);
 }
-
-/*
-int main(int argc, char *argv[])
-{
-    if(argc==3)
-    {
-        int N = atoi(argv[1]);
-        printf("%d\n",N);
-        ITER = atoi(argv[2]);
-        printf("%d\n",ITER);
-        pthread_t  *id_array;
-        id_array = (pthread_t*)malloc(sizeof(pthread_t)*N);
-        for(long j=0;j<N;j++)
-        {
-          pthread_create (&id_array[j], NULL, &function,(void*)j);
-        }
-        for(long k=0;k<N;k++)
-        {
-            pthread_join(id_array[k], NULL);
-        }
-        printf("\n\n %d \n\n", counter);
-        exit(EXIT_SUCCESS);
-    }
-*/
