a1a8e9fc688fbb754dd14473e4fae503b74001a9
Jakub Mroczkowski <jakub_mroczkowski@wp.pl>
2019-06-16 18:20:57 +0200

    Cleared

--- sh2/main.c
+++ sh2/main.c
@@ -9,9 +9,6 @@
 #include <time.h>
 #include "queue.h"
 
-////Waiting for X seconds in cond_timedwait will result in error
-//#define MAX_TIMED_WAIT 10
-
 #define ENABLE_SLEEP 0
 
 //1 - debug mode level 1
@@ -34,12 +31,6 @@ pthread_mutex_t	mBarberWokenUp = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cBarberWokenUp = PTHREAD_COND_INITIALIZER;
 int barberWokenUp = 0;
 
-/*
-pthread_mutex_t* mInvitedToCut;
-pthread_cond_t* cInvitedToCut;
-int* invitedToCut;
-*/
-
 pthread_mutex_t* mFinishedCutting;
 pthread_cond_t* cFinishedCutting;
 int* finishedCutting;
@@ -57,31 +48,35 @@ void waiting(int sec)
 
 void* Barber(void* arg)
 {
-	//int result;
+	//variable containing latest error code
+	int result;
 	while(1)
 	{
-		//Sprawdz raz kolejke
+		//Check waiting queue once
 		pthread_mutex_lock(&accessWaitingQueue);
 
-		//Ktoś jest w kolejce
+		//Someone is waiting
 		if(current_queue_size(&waitingQueue) > 0)
 		{
-			/*
-			//wyslij sygnal do klienta, ze bedzie go teraz strzygl
-			pthread_mutex_lock(&mInvitedToCut[currentlyCutId]);
-			invitedToCut[currentlyCutId] = 1;
-			pthread_cond_broadcast(&cInvitedToCut[currentlyCutId]);
-			pthread_mutex_unlock(&mInvitedToCut[currentlyCutId]);
-			*/
-			pthread_mutex_lock(&hairdressersChairTaken);
-			//Wez klienta
+			result = pthread_mutex_lock(&hairdressersChairTaken);
+			if (result)
+			{
+					fprintf(stderr, "hairdressersChairTaken could not be locked\n");
+					exit(EXIT_FAILURE);
+			}
+			//Take client
 			currentlyCutId = front(&waitingQueue);
 			pop(&waitingQueue);
 			//Something changed -> print full message here
-			//Strzyzenie
+			//Cutting...
 			if(debug >=2)
 				printf("Barber: cutting %d\n", currentlyCutId);
-			pthread_mutex_lock(&accessResignedQueue);
+			result = pthread_mutex_lock(&accessResignedQueue);
+			if (result)
+			{
+					fprintf(stderr, "accessResignedQueue could not be locked\n");
+					exit(EXIT_FAILURE);
+			}
 			if(debug)
 			{
 				printf("Resigned: ");
@@ -98,27 +93,30 @@ void* Barber(void* arg)
 			waiting(9);
 			if(debug >= 2)
 				printf("Barber: finished cutting %d\n", currentlyCutId);
-			//Skonczono strzyzenie
-			////Zmniejsz liczbe klientow
-			//pthread_mutex_lock(&accessTotalClientsCount);
-			//--totalClientsCount;
-			//pthread_mutex_unlock(&accessTotalClientsCount);
-
-			//Wyslij sygnal klientowi ze skonczyl go strzyc
-			pthread_mutex_lock(&mFinishedCutting[currentlyCutId]);
-			finishedCutting[currentlyCutId] = 1;
-			pthread_cond_broadcast(&cFinishedCutting[currentlyCutId]);
-			pthread_mutex_unlock(&mFinishedCutting[currentlyCutId]);
-			//Nikt nie jest teraz strzyzony - ustaw id na -1
+			//Finished
+			//Send signal to client that he can pay and leave
+			result = pthread_mutex_lock(&mFinishedCutting[currentlyCutId]);
+			if (result)
+			{
+					fprintf(stderr, "mFinishedCutting could not be locked\n");
+					exit(EXIT_FAILURE);
+			}
+			finishedCutting[currentlyCutId] = 1;		pthread_cond_broadcast(&cFinishedCutting[currentlyCutId]);										pthread_mutex_unlock(&mFinishedCutting[currentlyCutId]);
+			//Nobody is getting cut - set currentlyCutId to -1
 			pthread_mutex_lock(&hairdressersChairTaken);
 			currentlyCutId = -1;
 			pthread_mutex_unlock(&hairdressersChairTaken);
 		}
-		//Spij
+		//Sleep
 		else
 		{
-		    pthread_mutex_unlock(&accessWaitingQueue);
-			pthread_mutex_lock(&mBarberWokenUp);
+	    pthread_mutex_unlock(&accessWaitingQueue);
+			result = pthread_mutex_lock(&mBarberWokenUp);
+			if (result)
+			{
+					fprintf(stderr, "mBarberWokenUp could not be locked\n");
+					exit(EXIT_FAILURE);
+			}
 			barberWokenUp = 0;
 			while(barberWokenUp == 0)
 			{
@@ -130,8 +128,8 @@ void* Barber(void* arg)
 }
 
 void* Client(void* numer) {
-    int id;
-    id = *((int *) numer);
+  int id;
+  id = *((int *) numer);
 	if(debug >= 2)
 		printf("Created client %d\n", id);
 	int result;
@@ -143,10 +141,9 @@ void* Client(void* numer) {
 			fprintf(stderr, "accessWaitingQueue could not be locked\n");
 			exit(EXIT_FAILURE);
 		}
-		//Nie ma wolnych krzeseł - rezygnujemy
+		//Queue full - go home
 		if(current_queue_size(&waitingQueue) >= totalChairs)
 		{
-			//pthread_mutex_unlock(&accessWaitingQueue);
 			//Add client that resigned to resigned queue
 			result = pthread_mutex_lock(&accessResignedQueue);
 			if(result)
@@ -155,12 +152,7 @@ void* Client(void* numer) {
 				exit(EXIT_FAILURE);
 			}
 			push(&resignedQueue, id);
-
-			////pthread_mutex_lock(&accessTotalClientsCount);
-			////--totalClientsCount;
-			////pthread_mutex_unlock(&accessTotalClientsCount);
 			//Something changed -> print full message here
-			//pthread_mutex_lock(&accessWaitingQueue);	
 			pthread_mutex_lock(&hairdressersChairTaken);
 			if(debug)
 			{
@@ -181,14 +173,24 @@ void* Client(void* numer) {
 				printf("Client %d resigned\n", id);
 			return 0;
 		}
-		//Kolejka ma wolne miejsca - dołączamy do kolejki
+		//There is an empty chair - join queue
 		else
 		{
 			if(debug >= 2)
 				printf("Client joining queue, clent id: %d\n", id);
 			//Something changed -> print full message here
-			pthread_mutex_lock(&accessResignedQueue);
-			pthread_mutex_lock(&hairdressersChairTaken);
+			result = pthread_mutex_lock(&accessResignedQueue);
+			if (result)
+			{
+					fprintf(stderr, "accessResignedQueue could not be locked\n");
+					exit(EXIT_FAILURE);
+			}
+			result = pthread_mutex_lock(&hairdressersChairTaken);
+			if (result)
+			{
+					fprintf(stderr, "hairdressersChairTaken could not be locked\n");
+					exit(EXIT_FAILURE);
+			}
 			if(debug)
 			{
 				printf("Resigned: ");
@@ -204,28 +206,29 @@ void* Client(void* numer) {
 			push(&waitingQueue, id);
 			pthread_mutex_unlock(&accessWaitingQueue);
 
-			//wyslij sygnal ze ktos jest gotowy
-			pthread_mutex_lock(&mBarberWokenUp);
+			//wake up barber
+			result = pthread_mutex_lock(&mBarberWokenUp);
+			if (result)
+			{
+					fprintf(stderr, "mBarberWokenUp could not be locked\n");
+					exit(EXIT_FAILURE);
+			}
 			barberWokenUp = 1;
 			pthread_cond_broadcast(&cBarberWokenUp);
 			pthread_mutex_unlock(&mBarberWokenUp);
-			/*
-			//czekaj az zaprosi go fryzjer
-			pthread_mutex_lock(&mInvitedToCut[id]);
-			while(invitedToCut[id] == 0)
+			//wait untill barber says cutting is done
+			result = pthread_mutex_lock(&mFinishedCutting[id]);
+			if (result)
 			{
-				pthread_cond_wait(&cInvitedToCut[id], &mInvitedToCut[id]);
+					fprintf(stderr, "mFinishedCutting could not be locked\n");
+					exit(EXIT_FAILURE);
 			}
-			pthread_mutex_unlock(&mInvitedToCut[id]);
-			*/
-			//czekaj az fryzjer zasygnalizuje, ze skonczyl go strzyc
-			pthread_mutex_lock(&mFinishedCutting[id]);
 			while(finishedCutting[id] == 0)
 			{
 				pthread_cond_wait(&cFinishedCutting[id], &mFinishedCutting[id]);
 			}
 			pthread_mutex_unlock(&mFinishedCutting[id]);
-			//idz do domu
+			//go home
 			break;
 		}
 	}
@@ -256,60 +259,21 @@ int main(int argc, char* argv[])
 		else if(debug == 2)
 			printf("Debug level 2 enabled\n");
     //Set all chairs count
-        totalChairs = atoi(argv[1]);
+    totalChairs = atoi(argv[1]);
     if(totalChairs <= 0)
     {
       fprintf(stderr, "Invalid total chairs count\n");
       return 2;
     }
     //Set totalClientsCount
-        totalClientsCount = atoi(argv[2]);
+    totalClientsCount = atoi(argv[2]);
     if(totalClientsCount <= 0)
     {
       fprintf(stderr, "Invalid total clients count\n");
       return 3;
     }
-		/*
-		//alloc and init invitedToCut
-		mInvitedToCut = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*totalClientsCount);
-		if(mInvitedToCut == NULL)
-		{
-			fprintf(stderr, "Could not allocate memory\n");
-			return 9;
-		}
-		cInvitedToCut = (pthread_cond_t*)malloc(sizeof(pthread_cond_t)*totalClientsCount);
-		if(cInvitedToCut == NULL)
-		{
-			fprintf(stderr, "Could not allocate memory\n");
-			return 9;
-		}
-		invitedToCut = (int*)malloc(sizeof(int)*totalClientsCount);
-		if(invitedToCut == NULL)
-		{
-			fprintf(stderr, "Could not allocate memory\n");
-			return 9;
-		}
-		int i;
-		for(i=0;i<totalClientsCount;i++)
-		{
-			int mutexResult = pthread_mutex_init(&mInvitedToCut[i], NULL);
-			if(mutexResult)
-			{
-				fprintf(stderr, "Could not init mInvitedToCut[%d]\n", i);
-				return 10;
-			}
-			int condResult = pthread_cond_init(&cInvitedToCut[i], NULL);
-			if(condResult)
-			{
-				fprintf(stderr, "Could not init cInvitedToCut[%d]\n", i);
-				return 11;
-			}
-			//Client not invited at beginning
-			invitedToCut[i] = 0;
-		}
-		*/
 		//alloc and init finishedCutting
-				mFinishedCutting = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*totalClientsCount);
+		mFinishedCutting = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*totalClientsCount);
 		if(mFinishedCutting == NULL)
 		{
 			fprintf(stderr, "Could not allocate memory\n");
@@ -359,7 +323,7 @@ int main(int argc, char* argv[])
 		{
 			fprintf(stderr, "Could not create barber thread\n");
 		}
-    //Create clients threads
+    //Alloc memory for clients' threads
     pthread_t* clientID;
     clientID = (pthread_t*)malloc(sizeof(pthread_t)*totalClientsCount);
 		if(clientID == NULL)
@@ -376,12 +340,13 @@ int main(int argc, char* argv[])
 		if(debug >= 2)
 			printf("(debug) Program begins\n");
     int j;
-	/* utworzenie wątków */
-	for (j=0; j < totalClientsCount; j++) {
+	//Create clients threads
+	for (j=0; j < totalClientsCount; j++)
+	{
 		waiting(4);
-        threadID[j]=j;
+    threadID[j]=j;
 		int errCode = pthread_create (&clientID[j], NULL, &Client, (void*)&threadID[j]);
-        if(errCode!=0)
+      if(errCode!=0)
 			{
 				fprintf(stderr, "pthread_create returned value: %d, could not create thread number %d\n", errCode, j);
 				fprintf(stderr, "Make sure you did not try to create too many threads\n");
@@ -389,10 +354,10 @@ int main(int argc, char* argv[])
 			}
 	}
 
-    for(int j=0;j<totalClientsCount;j++)
-    {
-      pthread_join(clientID[j], NULL);
-    }
+  for(int j=0;j<totalClientsCount;j++)
+  {
+    pthread_join(clientID[j], NULL);
+  }
 	printf("Barber finished work for today.\n");
-    exit(EXIT_SUCCESS);
+  exit(EXIT_SUCCESS);
 }
